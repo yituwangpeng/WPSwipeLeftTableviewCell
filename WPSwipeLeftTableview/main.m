147cb5a7f2676e3807c30cecb289d6e94d93527f
wangpeng <yituwangpeng@gmail.com>
2015-10-09 15:53:20 +0800

    Initial Commit

---
+++ WPSwipeLeftTableview/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WPSwipeLeftTableview
+//
+//  Created by 王鹏 on 15/10/9.
+//  Copyright © 2015年 王鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
